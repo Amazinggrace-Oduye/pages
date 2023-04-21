793b46b3944c5137c2e882e85f86b57fe38fc539
Amazinggrace <grace.oduye@gmail.com>
2023-04-21 21:18:02 +0100

    deved full login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,124 +1,91 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import profilePic from "avarter.jpeg";
+import { FaRegEnvelope } from "react-icons/fa";
+import { MdLockOutline } from "react-icons/md";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col item-center justify-center min-h-screen py-2 bg-blue-200">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        {/* caousel and login section */}
+        <div className=" bg-white w-screen h-screen flex">
+          <div className="w-full h-full bg-blue-400">
+            <div className="text-left font-bold px-3 mt-4 text-white text-xl">
+              Cet<span className="text-blue-500">on</span>
+            </div>
+            <div className=" text-white px-10 mt-80 text-2xl">
+              <h2 className="font-bold">Hello, Friends!</h2>
+              <h2 className="font-bold">
+                I&apos;m using Centon to manage my business
+              </h2>
+              <h2 className="font-bold">
+                I can&apos;t imagine working without Cet
+                <span className="text-blue-500">on</span>
+              </h2>
+            </div>
+            <div className="relative">
+              <Image
+                className="rounded-full relative mx-auto my-6"
+                alt="user image"
+                src={profilePic}
+                width={50}
+                height={50}
+              />
+              <h6 className="font-bold">Kelly Williams</h6>
+              <h6 className=" text-gray-200 text-base font-bold">
+                CEO, Business 1
+              </h6>
+              <p>⭐⭐⭐⭐⭐✨</p>
+            </div>
+          </div>
+          <div className="w-full h-full text-black">
+            <div className="py-5 mt-60 justify-center text-left ml-40 ">
+              <h1 className="font-bold text-2xl ">Welcome back</h1>
+              <p className="font-light  text-gray-500 ">
+                Welcome back! Please enter your detail.
+              </p>
+            </div>
+            <div className=" flex flex-col ml-40">
+              <p className="text-left font-light">Email</p>
+              <div className="w-1/2 p-1.5 bg-gray-50 border-2 rounded-md flex">
+                <FaRegEnvelope className="mr-2 text-gray-500" />
+                <label htmlFor="email"></label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  pattern="[a-z0-9]{1,15}"
+                  title="Should be a valid email."
+                  placeholder="Enter your email"
+                  className="outline-none bg-white text-sm flex-auto"
+                />
+              </div>
+              <p className="text-left mt-3 font-light">Password</p>
+              <div className="w-1/2 p-1.5 bg-white border-2 rounded-md flex ">
+                <MdLockOutline className="mr-2 text-gray-500" />
+                <label htmlFor="password"></label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+                  title="Password should be digits (0 to 9) special character and alphabets (a to z)."
+                  placeholder="Enterl your password"
+                  className="outline-none bg-white text-sm flex-auto"
+                />
+              </div>
+              <div className="w-1/2 p-1.5 mt-5 bg-blue-400 border-2  rounded-md flex ">
+                <button type="submit" className="mx-auto text-white">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+    </div>
+  );
 }
